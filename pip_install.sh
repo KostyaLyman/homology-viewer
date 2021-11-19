a5fcb3bd0e6178b257ac0531153b7ddf1c0b6de3
KostyaLyman <lyman.kostiantyn@gmail.com>
2021-11-19 00:56:07 -0800

    install dash+plotly

---
+++ pip_install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+python3 -m venv env
+source env/bin/activate
+pip install --upgrade pip
+python --version
+which python
+pip --version
+
+pip install numpy
+pip install scipy
+pip install networkx
+pip install pandas
+pip install -U matplotlib;
+pip install notebook
+
+pip install plotly
+pip install dash
+pip install dash-bootstrap-components
+
+pip install colour
+
+printf "\n Hooray! \n"
