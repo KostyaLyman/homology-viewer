434b62a4dcbd360bd5388f960079091483ff568c
KostyaLyman <lyman.kostiantyn@gmail.com>
2021-12-13 21:05:42 -0500

    - refactor & improve UI

--- hv-dash-app.py
+++ hv-dash-app.py
@@ -65,7 +65,7 @@ scx.main_figure = scx.setup_fig(scx.main_layout)
 #                 prevent_initial_callbacks=False
 #                 )
 app = DashProxy(__name__,
-                external_stylesheets=[dbc.themes.FLATLY],
+                external_stylesheets=[dbc.themes.CERULEAN],
                 # external_stylesheets=[dbc.themes.VAPOR],
                 prevent_initial_callbacks=False,
                 transforms=[MultiplexerTransform()]
@@ -90,7 +90,191 @@ main_checklist_options = [
 
 
 # -----------------------------------------------------------------------------
-#                 ===== CALLBACKS =====
+#                 ===== LAYOUT / COMPONENTS =====
+# -----------------------------------------------------------------------------
+buttons_row = dbc.Row(children=[
+    dbc.Col(width=3,
+            children=[
+                # html.P(),
+                dbc.Button("Random", id='random-button', color='primary', class_name='me-1'),
+                html.P(),
+                dbc.Input(id='random-size-input', type="number",
+                          value=15, min=3, max=50,
+                          # style={'width': 100}
+                          class_name='me-1'
+                          ),
+                # html.P()
+            ], align='center'),
+    dbc.Col(width={"size": 2},
+            children=[
+                # html.P(),
+                dbc.Button("Reset", id='reset-button', class_name='me-1', color='danger',
+                            # style={'width': 83}
+                            ),
+                html.P(),
+                dbc.Button("Science", id='sci-button', class_name='me-1', color='info',
+                            style={'text-align': 'center'}),
+                # html.P(),
+            ], align='center'),
+    dbc.Col(width={"size": 2},
+            children=[
+                # html.P(),
+                dbc.Button("Load", id='load-button', color='warning'),
+                html.P(),
+                dbc.Button("Save", id='save-button', color='success'),
+                # html.P()
+            ], align='center')
+], justify="around", class_name='mt-0 mb-0')
+
+buttons_card = dbc.Card(children=[
+    dbc.CardHeader("Point cloud"),
+    dbc.CardBody([buttons_row])
+], class_name='mt-0 mb-0', color="primary", outline=True)
+
+
+check_radio_row = dbc.Row(children=[
+    dbc.Col(children=[
+        dcc.Checklist(
+            id='main-checklist',
+            options=main_checklist_options,
+            value=[],
+            # labelStyle={"display": "block"},
+            labelStyle={"display": "inline-block",
+                        # 'justify-content': 'space-between',
+                        'width': '25%',
+                        'margin': '.5em'},
+            inputClassName='form-check-input',
+            labelClassName='form-check-label',
+            # className="form-check form-switch"
+            className="form-check"
+        ),
+        dcc.RadioItems(
+            id='main-radio',
+            options=main_checklist_options,
+            value=scx.ST_POINT,
+            labelStyle={"display": "inline-block",
+                        # 'justify-content': 'space-between',
+                        'width': '25%',
+                        'margin': '.5em'},
+            inputClassName='form-check-input',
+            labelClassName='form-check-label',
+            className="form-check"
+        )
+    ])],
+    style={'border': 'thin lightgrey solid'}
+)
+
+points_tab_row = dbc.Row(children=[
+    dbc.Col(width=4, children=[
+        # html.Div(id='selected-points-div', className='alert-inf', children=["asd"])
+        html.P(),
+        dbc.Card(id='selected-points-div', children=[
+            dbc.CardHeader("selected"),
+            dbc.CardBody([
+                # html.H6("Points selected", className="card-title"),
+                html.P(id='selected-points-size', className='card-text', children=["# "])
+            ])
+        ])
+    ]),
+    dbc.Col(width={'size': 4}, children=[
+        html.P(),
+        dbc.Button("Delete points", id='delete-points-button', color='danger')
+    ])
+], class_name='mb-0')
+
+edges_tab_row = dbc.Row(children=[
+    dbc.Col(width=4, children=[
+        # html.Div(id='selected-edges-div', className='alert alert-inf', children=["qwe"])
+        html.P(),
+        dbc.Card(id='selected-edges-div', children=["qwe"], body=True)
+    ]),
+    dbc.Col(width=4, children=[
+        html.P(),
+        dbc.Button("Make chain", id='make-chain-button', color='primary')
+    ])
+], className='mb-0')
+
+tris_tab_row = dbc.Row(children=[
+    dbc.Col(width=8, children=[
+        html.P(),
+        dbc.Card(children=[
+            dbc.CardHeader("Stats"),
+            dbc.CardBody(id='tris-stats-card')
+        ])
+    ]),
+    dbc.Col(width=4, children=[
+        html.P(),
+        dbc.Button("Make holes", id='make-holes-button', color='primary')
+    ])
+], className='mb-0')
+
+stat_tabs_row = dbc.Row([dbc.Col([
+
+    # dbc.Card([
+        dbc.Tabs(id='stat-tabs', active_tab=scx.ST_POINT, class_name='nav-pills nav-justified',
+                 # className='nav nav-tabs',  parent_className='nav nav-tabs',
+                 children=[
+                     dbc.Tab(label='Points', tab_id=scx.ST_POINT, children=[points_tab_row],
+                             # active_tab_class_name='nav-item warning'
+                             # active_tab_style={"background-color": "green", "color":  "#17a2b8"}
+                             ),
+                     dbc.Tab(label='Edges', tab_id=scx.ST_EDGE, children=[edges_tab_row]),
+                     dbc.Tab(label='Triangles', tab_id=scx.ST_TRI, children=[tris_tab_row])
+                 ])
+    # ])
+])
+])
+
+stat_tabs_card = dbc.Card(children=[
+    # dbc.CardHeader(tabs_row),
+    # dbc.CardBody(html.Div(id='tabs-content-div'))
+    dbc.CardBody(stat_tabs_row)
+], className='mt-0 mb-0', color="info", outline=True)
+
+main_tabs_content = {
+    'main-tab-scx': [
+        buttons_card,
+        html.P(),
+        stat_tabs_card,
+    ],
+    'main-tab-ntk': [
+        html.P("some content")
+    ]
+}
+
+main_tabs_row = dbc.Row([dbc.Col([
+    dbc.Tabs(id='main-tabs', active_tab='main-tab-scx', class_name='nav-justified',
+             children=[
+                 dbc.Tab(label='2D Complex', tab_id='main-tab-scx',
+                         # class_name='mr-0 ml-0 pr-0 pl-0',
+                         # label_class_name='mr-0 ml-0 pr-0 pl-0',
+                         label_class_name='text-warning',
+                         # style={'width': '100px', 'padding': '0'}
+                         ),
+                 dbc.Tab(label='Network', tab_id='main-tab-ntk', label_class_name='text-success')
+             ])
+])])
+
+main_tabs_card = dbc.Card([
+    dbc.CardHeader(main_tabs_row, class_name="mb-0 ml-6 pl-5"),
+    # dbc.CardBody(html.Div(id='main-tabs-content-div'))
+    dbc.CardBody([dbc.Row([dbc.Col([
+        dbc.Collapse(id='main-tab-scx-collapse', is_open=True, children=main_tabs_content['main-tab-scx']),
+        dbc.Collapse(id='main-tab-ntk-collapse', is_open=False, children=main_tabs_content['main-tab-ntk'])
+    ])])])
+], className='mt-0 mb-0', color="success", outline=True)
+
+click_data_row = dbc.Row(html.Div(
+    children=[
+        dcc.Markdown(dedent("""**Click Data**""")),
+        html.Pre(id='click-data', style=styles['pre'])
+    ],
+    style={'height': '800px', 'width': '400px'}
+))
+
+
+# -----------------------------------------------------------------------------
+#                 ===== LAYOUT / APP =====
 # -----------------------------------------------------------------------------
 app.layout = html.Div([
     # Title -----------------------------------------------
@@ -104,95 +288,55 @@ app.layout = html.Div([
                 width=7,
                 children=[
                     dcc.Graph(id="main-figure")
-                ], style={'overflowY': 'scroll', 'overflowX': 'scroll', 'height': 600}),
+                ], style={'overflowY': 'scroll', 'overflowX': 'scroll', 'height': 800, 'width': 800},
+                className='pr-4'
+            ),
             # Right Side ----------------------------------
             dbc.Col(
-                width=4,
+                width=5,
                 children=[
                     # Buttons -----------------------------
-                    dbc.Row(
-                        children=[
-                            dbc.Col(
-                                width=4,
-                                children=[
-                                    html.Button("Random cloud", id='random-button',
-                                                className='btn btn-info'),
-                                    dcc.Input(id='random-size-input', type="number",
-                                              placeholder='number of vertices')
-                                ]),
-                            dbc.Col(
-                                width={"size": 3, "offset": 2},
-                                children=[
-                                    html.Button("Reset", id='reset-button',
-                                                className='btn btn-primary'),
-                                    html.Button("Science", id='sci-button',
-                                                className='btn btn-warning',
-                                                style={'text-align': 'center'})
-                                ])
-                        ]),
+                    # buttons_card,
                     # Checklist ---------------------------
-                    html.P(),
-                    dbc.Row(html.Div(
-                        # style={'border': 'thin lightgrey solid', 'width': '400px'},
-                        style={'border': 'thin lightgrey solid'},
-                        # className="form-check form-switch",
-                        children=[
-                            dcc.Checklist(
-                                id='main-checklist',
-                                options=main_checklist_options,
-                                value=[],
-                                # labelStyle={"display": "block"},
-                                labelStyle={"display": "inline-block",
-                                            # 'justify-content': 'space-between',
-                                            'width': '25%',
-                                            'margin': '.5em'},
-                                inputClassName='form-check-input',
-                                labelClassName='form-check-label',
-                                # className="form-check form-switch"
-                                className="form-check"
-                            ),
-                            dcc.RadioItems(
-                                id='main-radio',
-                                options=main_checklist_options,
-                                value=scx.ST_POINT,
-                                labelStyle={"display": "inline-block",
-                                            # 'justify-content': 'space-between',
-                                            'width': '25%',
-                                            'margin': '.5em'},
-                                inputClassName='form-check-input',
-                                labelClassName='form-check-label',
-                                className="form-check"
-                            )
-                        ])),
+                    # html.P(),
+                    # check_radio_row,
+                    # Tabs --------------------------------
+                    # html.P(),
+                    # stat_tabs_card,
+                    main_tabs_card,
                     # Click Data --------------------------
                     html.P(),
-                    dbc.Row(html.Div(
-                        style={'height': '800px', 'width': '400px'},
-                        children=[
-                            dcc.Markdown(dedent("""
-                            **Click Data**
-                            """)),
-                            html.Pre(id='click-data', style=styles['pre'])
-                        ]))
+                    click_data_row
                 ])
-        ])
+        ], justify="evenly")
 ])
+
+
+# -----------------------------------------------------------------------------
+#                 ===== CALLBACKS =====
+# -----------------------------------------------------------------------------
 @app.callback(
     output=[Output('click-data', 'children'),
-            Output('main-figure', 'figure')],
+            Output('main-figure', 'figure'),
+            Output('selected-points-size', 'children'),
+            # Output('tris-stats-card', 'children')
+            ],
     inputs=[Input('main-figure', 'clickData'),
             Input('main-figure', 'selectedData'),
             Input('reset-button', 'n_clicks'),
-            Input('main-radio', 'value')]
+            State('stat-tabs', 'active_tab')]
 )
 def display_click_data(clickData, selectData, reset_click, stype):
     print(f"%%% select data %%% {selectData and selectData['points']}")
+    seleced_points = 0
+    tris_table = dbc.Table()
     if selectData and selectData['points']:
         snames = [pts['id'] for pts in selectData['points']]
         print(f"%%% select data %%% {snames}")
 
         if stype == scx.ST_POINT:
             pnames = set(scx.filter_stype(snames, scx.ST_POINT))
+            seleced_points = len(pnames)
             scx.highlight_points(pnames)
 
         if stype == scx.ST_EDGE:
@@ -202,6 +346,11 @@ def display_click_data(clickData, selectData, reset_click, stype):
         if stype == scx.ST_TRI:
             tnames = set(scx.filter_stype(snames, scx.ST_TRI))
             scx.highlight_triangles(tnames)
+            tris_stats = pd.DataFrame(dict(stats={
+                '# triangles': len(tnames),
+                'area': 10
+            }))
+            tris_table = dbc.Table.from_dataframe(tris_stats, bordered=True, hover=True, index=True)
 
     else:
         scx.clear_highlighting()
@@ -213,24 +362,56 @@ def display_click_data(clickData, selectData, reset_click, stype):
                 "\n@@@ magic @@@\n" + \
                 json.dumps({"magic": reset_click})
 
-    return json_dump, scx.get_main_figure()
+    return json_dump, scx.get_main_figure(), seleced_points
 
 
 @app.callback(
-    output=[Output('main-figure', 'figure'), Output('main-figure', 'selectedData')],
-    inputs=Input('main-radio', 'value')
+    output=[Output('main-figure', 'figure'),
+            Output('main-figure', 'selectedData')],
+    # inputs=Input('main-radio', 'value')
+    inputs=Input('stat-tabs', 'active_tab')
 )
-def radio_callback(radio_value):
-    print(f"%%% radio %%% {radio_value}")
+def stat_tabs_to_figure(active_tab):
+    print(f"%%% radio %%% {active_tab}")
     scx.clear_highlighting()
-    scx.show_hide_points(radio_value, mode='opacity')
+    scx.show_hide_points(active_tab, mode='opacity')
+
     # scx.show_obscure_points(radio_value)
     return scx.get_main_figure(), dict(points=[])
 
 
+# @app.callback(
+#     output=Output('tabs-content-div', 'children'),
+#     inputs=Input('stat-tabs', 'active_tab')
+# )
+# def stat_tabs_to_content(active_tab):
+#     tabs_content = {
+#         scx.ST_POINT: points_tab_row,
+#         scx.ST_EDGE: edges_tab_row,
+#         scx.ST_TRI: tris_tab_row
+#     }
+#     return tabs_content.get(active_tab, [])
+
 @app.callback(
-    output=[Output('main-figure', 'figure'), Output('main-radio', 'value')],
-    inputs=[Input('random-button', 'n_clicks'), State('random-size-input', 'value')]
+    output=[Output('main-tab-scx-collapse', 'is_open'),
+            Output('main-tab-ntk-collapse', 'is_open')],
+    inputs=Input('main-tabs', 'active_tab')
+)
+def main_tabs_to_content(active_tab):
+    tab_content = {
+        'main-tab-scx': (True, False),
+        'main-tab-ntk': (False, True)
+    }
+
+    tab_scx, tab_ntk = tab_content.get(active_tab, (False, False))
+    return tab_scx, tab_ntk
+
+
+@app.callback(
+    output=[Output('main-figure', 'figure'),
+            Output('stat-tabs', 'active_tab')],
+    inputs=[Input('random-button', 'n_clicks'),
+            State('random-size-input', 'value')]
 )
 def random_cloud(rnd_click, rnd_size):
     print(f"\n================\n" +
@@ -247,18 +428,6 @@ def random_cloud(rnd_click, rnd_size):
 
 @app.callback(
     output=Output('main-figure', 'figure'),
-    inputs=[Input('reset-button', 'n_clicks')]
-)
-def reset_figure(reset_click):
-    print(f"\n================\n" +
-          f" [{reset_click}] : reset_figure " +
-          f"\n================\n")
-    scx.main_figure = scx.reset_fig()
-    return scx.get_main_figure()
-
-
-@app.callback(
-    output=Output('main-figure', 'figure'),
     inputs=[Input('sci-button', 'n_clicks')]
 )
 def triangulate_cloud(sci_click):
@@ -269,6 +438,18 @@ def triangulate_cloud(sci_click):
     if sci_click is not None:
         scx.triangulate()
 
+    return scx.get_main_figure()
+
+
+@app.callback(
+    output=Output('main-figure', 'figure'),
+    inputs=[Input('reset-button', 'n_clicks')]
+)
+def reset_figure(reset_click):
+    print(f"\n================\n" +
+          f" [{reset_click}] : reset_figure " +
+          f"\n================\n")
+    scx.main_figure = scx.reset_fig()
     return scx.get_main_figure()
 
 # -----------------------------------------------------------------------------
